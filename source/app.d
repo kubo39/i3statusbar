9210da762c685464939f62408f2684a636b93c23
Hiroki Noda <kubo39@gmail.com>
2018-05-19 08:20:46 +0900

    Cleanup

--- source/app.d
+++ source/app.d
@@ -7,7 +7,7 @@ protocol reference: https://i3wm.org/docs/i3bar-protocol.html
 import core.time : seconds;
 import core.thread : Thread;
 import std.conv : to;
-import std.file : readText;
+import std.file : readText, slurp;
 import std.format : format;
 import std.json : JSONValue, toJSON;
 import std.stdio : writeln;
@@ -22,32 +22,40 @@ enum
     YELLOW = "#FFFF00",
 }
 
+/**
+battery information: https://www.kernel.org/doc/Documentation/power/power_supply_class.txt
+ */
 JSONValue getBatteryStatus()
 {
     JSONValue jj = ["name": "battery"];
-    auto energy = readText("/sys/class/power_supply/BAT0/energy_now").chop().to!ulong;
-    auto power = readText("/sys/class/power_supply/BAT0/power_now").chop().to!ulong;
+    auto energyFull = slurp!ulong("/sys/class/power_supply/BAT0/energy_now", "%d")[0];
+    auto energyNow = slurp!ulong("/sys/class/power_supply/BAT0/energy_full", "%d")[0];
+    auto powerNow = slurp!ulong("/sys/class/power_supply/BAT0/power_now", "%d")[0];
+    auto capacity = slurp!ulong("/sys/class/power_supply/BAT0/capacity", "%d")[0];
 
-    if (power == 0)
+    // charging
+    if (powerNow == 0)
     {
         jj.object["color"] = GREEN;
-        jj.object["full_text"] = "Battery:charging..";
+        jj.object["full_text"] = format("Battery: %d %% (charging...)", capacity);
         return jj;
     }
 
     auto status = readText("/sys/class/power_supply/BAT0/status").chop();
     if (status.cmp("Discharging"))
     {
-        auto time = energy.to!double / power.to!double;
+        auto time = energyNow.to!double / powerNow.to!double;
         auto minutes = (time * 60).to!ulong;
         auto hours = minutes / 60;
         auto minute = minutes % 60;
         jj.object["color"] = YELLOW;
         jj.object["full_text"] = format("Battery: %d h %d min.", hours, minute);
-        return jj;
     }
-    jj.object["color"] = GREEN;
-    jj.object["full_text"] = "Battery:full";
+    else // status == "Full"
+    {
+        jj.object["color"] = GREEN;
+        jj.object["full_text"] = format("Battery: %d%%", capacity);
+    }
     return jj;
 }
 
